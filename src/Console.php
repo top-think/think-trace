9d35d83f850e74510766ab201500b3a96934bdab
thinkphp <thinkphp@qq.com>
2019-10-02 21:53:12 +0800

    改进session信息显示

--- src/Console.php
+++ src/Console.php
@@ -65,7 +65,7 @@ class Console
             '缓存信息' => $app->cache->getReadTimes() . ' reads,' . $app->cache->getWriteTimes() . ' writes',
         ];
 
-        if ($app->session->getId(false)) {
+        if (isset($app->session)) {
             $base['会话信息'] = 'SESSION_ID=' . $app->session->getId();
         }
 
